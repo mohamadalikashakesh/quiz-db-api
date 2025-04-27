dddb811430cc7839b2b4558f7ee132240e1fea05
mohamadalik92 <mohamadalikashakesh92@gmail.com>
2025-04-27 21:27:08 +0300

    db connection

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
 <?php
-require 'config.php';
+header('Content-Type: application/json');
+$host = 'localhost';
+$db   = 'quiz';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-
-
-
-
-
-
-
-
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    exit(json_encode(['error' => $e->getMessage()]));
+}
 ?>
