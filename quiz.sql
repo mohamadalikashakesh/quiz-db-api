26f3c5c1b9e0e3835b26a5a605287812b3cd0bf2
mohamadalik92 <mohamadalikashakesh92@gmail.com>
2025-04-27 21:04:52 +0300

    added previlige to user table

--- quiz.sql
+++ quiz.sql
@@ -2,7 +2,9 @@ CREATE TABLE Users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+    password_hash VARCHAR(255) NOT NULL,
+    privileges ENUM('user', 'admin') DEFAULT 'user'
+
 );
 
 CREATE TABLE Categories (
